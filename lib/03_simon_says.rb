158236342a6dd5e4d30cba72234cecf2b0e7fe70
Joseph <joseph.jr@gmail.com>
2016-11-22 18:42:08 +0000

    simon says methods

--- lib/03_simon_says.rb
+++ lib/03_simon_says.rb
@@ -1,19 +1,23 @@
 def echo(x)
-
 x
-
 end
 
 def shout(x)
-
   x.upcase
 end
 
 def repeat(x, y = nil)
+  if y == nil
+    x + " " + x
+  else
+    result = []
+    y.times do
+      result.push(x)
+    end
+    result.join(" ")
+  end
+end
 
-if y == nil
-  x + " " + x
-else
-
-
+def start_of_word(x, y = nil)
+  x[0..y-1]
 end
